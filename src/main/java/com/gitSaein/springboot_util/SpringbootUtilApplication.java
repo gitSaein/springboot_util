3b112eff2c35ce88830ca559e97d9c93d2957396
saein.lee <gitSaein@gmail.com>
2019-09-22 12:57:42 +0900

    init mail

---
+++ src/main/java/com/gitSaein/springboot_util/SpringbootUtilApplication.java
@@ -0,0 +1,13 @@
+package com.gitSaein.springboot_util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUtilApplication.class, args);
+	}
+
+}
