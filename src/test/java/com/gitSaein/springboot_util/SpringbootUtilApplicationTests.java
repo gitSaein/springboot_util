9f4199c2c2b16a0c3de36d88758336815f38ba91
saein.lee <gitSaein@gmail.com>
2019-12-29 14:53:45 +0900

    gradle init

---
+++ src/test/java/com/gitSaein/springboot_util/SpringbootUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitSaein.springboot_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
